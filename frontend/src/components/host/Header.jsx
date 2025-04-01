edec62c7e6b14b6acbcca0945985aed585f3b5d8
ChinhDe <chinhde29@gmail.com>
2025-04-01 11:58:07 +0700

    update avatar

--- frontend/src/components/host/Header.jsx
+++ frontend/src/components/host/Header.jsx
@@ -1,11 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import AuthContext from "@context/AuthContext";
+import useAuth from "@/hooks/useAuth";
+import axiosInstance from "@/utils/axiosInstance";
 
 const Header = () => {
   const navigate = useNavigate();
   const { authTokens, logoutUser, user } = useContext(AuthContext);
+  const isAuthenticated = useAuth();
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      axiosInstance
+        .get("/profile/avatar")
+        .then((response) => {
+          console.log("API Response:", response.data);
+          setAvatar(response.data.avatar_url);
+        })
+        .catch((error) => {
+          console.error("Error fetching profile:", error);
+        });
+    }
+  }, [isAuthenticated]);
   
   const handleLogout = () => {
     logoutUser();
@@ -19,11 +37,12 @@ const Header = () => {
           {/* Logo */}
           <a href="#" className="flex items-center">
             <img
-              src="https://i.pravatar.cc/200"
+              src="https://sdmntpreastus2.oaiusercontent.com/files/00000000-a32c-51f6-933a-d944ea1017ab/raw?se=2025-04-01T05%3A50%3A01Z&sp=r&sv=2024-08-04&sr=b&scid=54846a39-6dc6-58fe-95bd-b8e2dd5da4c7&skoid=a47cd303-16a2-427e-8efb-2ce406116005&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-31T06%3A33%3A26Z&ske=2025-04-01T06%3A33%3A26Z&sks=b&skv=2024-08-04&sig=ctyQd4PG3yhG%2BjSEaBOlRyVuyn5VVjWM0w4PWIeggSE%3D"
               alt="Logo"
-              className="h-8 mr-3"
+              className="h-10 mr-3 rounded-full scale-150"
+
             />
-            <span className="text-xl font-semibold text-gray-900">Homestay App</span>
+            <span className="text-xl font-semibold text-red-500">Homestay App</span>
           </a>
 
           {/* Menu Desktop */}
@@ -66,7 +85,7 @@ const Header = () => {
                   className="flex items-center text-gray-700 hover:text-blue-600 focus:outline-none"
                 >
                   <img
-                    src={user?.avatar || "https://i.pravatar.cc/40"}
+                    src={avatar || "https://i.pravatar.cc/40"}
                     alt="User Avatar"
                     className="w-8 h-8 rounded-full mr-2"
                   />
